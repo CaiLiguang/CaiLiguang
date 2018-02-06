dc58dca783fd37b0ae73a6100f6b45f9c854a455
CaiLiguang <731994627@qq.com>
2018-02-06 15:01:38 +0800

    学习go语言常量iota的用法

--- test/goConst.go
+++ test/goConst.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "unsafe"
+// import "unsafe"
 
 // func main() {
 // 	const LENGTH int = 10
@@ -21,21 +21,54 @@ import "unsafe"
 // 	)
 // 	println(Female)
 // }
-const (
-    Unknown = 0
-    Female = 1
-    Male = 2
-)
-const (
-    a = "abc"
-    b = len(a)
-    c = unsafe.Sizeof(a)
-)
-// println(Female)
 
-//2.fmt.Printf不能打印整形
-func main() {
-	fmt.Printf("%d",Unknown)
-    println(a, b, c)
+// unsafe.Sizeof 函数
+//     t1 byte    1
+//     t2 int32   4
+//     t3 int64   8
+//     t4 string  16
+//     t5 bool    1
+// const (
+//     Unknown = 0
+//     Female = 1
+//     Male = 2
+// )
+// const (
+//     a = "123"
+//     b = len(a)
+//     c = unsafe.Sizeof(true)
+// )
+// // println(Female)
+
+// //2.fmt.Printf不能打印整形
+// func main() {
+// 	fmt.Printf("%d",Unknown)
+//     println(a, b, c)
+// }
+
+
+const (
+    i=1<<iota //0
+	j=3<<iota //11 向左移1位 110 转换10进制 6    
+	k=3<<2     	  //2 向左移2位 1100 转换10进制 12
+	l=3<<3 		  //3  向左移3位 11000 转换10进制 24
+
+    Active = 1 << iota //4 向左移4位 10000 转换10进制 16
+    Send	//5 向左移5位 100000 转换10进制 32
+    Receive	//6 向左移6位 1000000 转换10进制 64
+)  
+func main() {    
+	fmt.Println("i=",i)   
+	fmt.Println("j=",j)   
+	fmt.Println("k=",k)   
+	fmt.Println("l=",l) 
+	fmt.Println(Active, Send, Receive)
+
 }
+
+// const (
+    
+// )
+// func main() {
+// }
 

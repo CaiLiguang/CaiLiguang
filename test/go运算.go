5c7b2eb97e49cf51f20f3bbffebff1e6925d6209
CaiLiguang <731994627@qq.com>
2018-02-07 23:28:49 +0800

    add go 运算

---
+++ test/go运算.go
@@ -0,0 +1,30 @@
+package main
+
+// import "fmt"
+
+var a int= 1
+var b int= 2
+var c int
+func main() {
+	//1. c = a + b
+	// println(c)
+	// c = a - b
+	// println(c)
+	// c = a * b
+	// println(c)
+	// c = a / b
+	// println(c)
+	// c = a % b
+	// println(c)
+	// a++
+	// println(a)
+	// a--
+	// println(a)
+
+	if(a == b){
+		println(111)
+	} else {
+		println(222)
+
+	}
+}

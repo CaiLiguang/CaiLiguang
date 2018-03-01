5173ed8fc0a79fab341219c196833de2afab4a84
CaiLiguang <731994627@qq.com>
2018-03-01 18:41:03 +0800

    go函数

--- test/go函数.go
+++ test/go函数.go
@@ -1,15 +1,36 @@
 package main
-import "fmt"
+import  (
+	"fmt"
+	"math"
+	)
 
 func main() {
 	//定义局部变量
 	var a int = 100
 	var b int = 200
-	var ret int
+	// var ret int
 
-	//调用函数并返回最大值
-	ret = max(a,b)
-	fmt.Printf("最大值是：%d\n" , ret)
+	// //调用函数并返回最大值
+	// ret = max(a,b)
+	// fmt.Printf("最大值是：%d\n" , ret)
+	fmt.Printf("交换前a的值为：%d\n" , a)
+	fmt.Printf("交换前b的值为：%d\n" , b)
+
+	//通过调用函数来交换值
+	swap(&a,&b)
+	fmt.Printf("交换后a的值：%d\n" , a)
+	fmt.Printf("交换后b的值：%d\n" , b)
+    fmt.Println(a, b)
+
+    //求平方根
+    //getSquareRoot := func(x float64) float64{
+	get := func(x float64) float64{
+    	return math.Sqrt(x)
+    }
+
+    //使用平方根喊护士
+    fmt.Println(get(16))
+ 
 }
 
 //函数返回两个数的最大值
@@ -22,4 +43,13 @@ func max(num1 , num2 int) int {
 		result = num2
 	}
 	return result
+}
+
+//定义相互交换值的函数
+func swap(x *int, y *int) int {
+	var temp int
+	temp = *x //保存x的值
+	*x = *y //將y值赋给x
+	*y = temp //將temp值赋给y
+	return temp
 }
---
+++ test/go函数方法.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+//定义函数
+type Circle struct {
+	radius float64
+}
+
+func main() {
+	var c1 Circle
+	c1.radius = 10.00
+	fmt.Println("Area of Circle(c1) = ",c1.getArea())
+}
+
+//改method属于Circle类型对象中的方法
+func (c Circle) getArea() float64 {
+	//c.radius即为Circle类型对象中的属性
+	return 3.14 * c.radius * c.radius
+}
